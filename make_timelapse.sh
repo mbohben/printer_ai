9d64f976eb9530f7dc551430d0a4631e70b11ff4
mbohben <diankleng@gmail.com>
2026-04-21 18:45:01 +0700

    Delete make_timelapse.sh

--- make_timelapse.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p ~/printer_ai/timelapse
-
-ffmpeg -y -framerate 30 \
--pattern_type glob -i '/home/pi/printer_ai/buffer/*.jpg' \
--c:v libx264 -preset veryfast -crf 23 \
-/home/pi/printer_ai/timelapse/$(date +%s).mp4
